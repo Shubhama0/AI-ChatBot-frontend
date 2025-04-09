e3e5b0703f84a5cbc27c6d81084d21821c7a7065
Shubham Kumar <110241594+Shubhama0@users.noreply.github.com>
2025-04-09 15:51:42 +0530

    Update Verify.jsx

--- src/pages/Verify.jsx
+++ src/pages/Verify.jsx
@@ -20,18 +20,19 @@ const Verify = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <form
-        className="bg-white p-6 rounded shadow-md w-full
+        className="bg-indigo-300 p-6 rounded shadow-md w-full
      md:w-[500px]"
         onSubmit={submitHandler}
       >
-        <h2 className="text-2xl mb-4">Verify</h2>
+        <h2 className="text-2xl mb-4">Verification</h2>
         <div className="mb-4">
           <label className="block text-gray-700 mb-2" htmlFor="otp">
-            Otp:
+            OTP:
           </label>
           <input
             type="number"
             id="otp"
+            placeholder="enter your OTP "
             value={otp}
             onChange={(e) => setOtp(e.target.value)}
             className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
@@ -39,8 +40,8 @@ const Verify = () => {
           />
         </div>
 
-        <button className="bg-blue-500 text-white  py-2 px-4 rounded hover:bg-blue-700">
-          {btnLoading ? <LoadingSpinner /> : "Submit"}
+        <button className="bg-amber-500 text-white  py-2 px-4 rounded hover:bg-blue-700">
+          {btnLoading ? <LoadingSpinner /> : "Login"}
         </button>
       </form>
     </div>
