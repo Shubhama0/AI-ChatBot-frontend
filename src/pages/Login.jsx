3926328ea8e2d5f576e6a79c659ed1cc5091d4df
Shubham Kumar <110241594+Shubhama0@users.noreply.github.com>
2025-04-09 15:34:22 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,11 +17,11 @@ const Login = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <form
-        className="bg-white p-6 rounded shadow-md w-full
+        className="bg-indigo-300 p-6 rounded shadow-md w-full
      md:w-[500px]"
         onSubmit={submitHandler}
       >
-        <h2 className="text-2xl mb-4">Login</h2>
+        <h2 className="text-2xl mb-4">Welcome to AI-ChatBot</h2>
         <div className="mb-4">
           <label className="block text-gray-700 mb-2" htmlFor="email">
             Email:
@@ -29,6 +29,7 @@ const Login = () => {
           <input
             type="email"
             id="email"
+            placeholder="enter your Email "
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
@@ -37,10 +38,10 @@ const Login = () => {
         </div>
 
         <button
-          className="bg-blue-500 text-white  py-2 px-4 rounded hover:bg-blue-700"
+          className="bg-amber-500 text-white  py-2 px-4 rounded hover:bg-blue-700"
           disabled={btnLoading}
         >
-          {btnLoading ? <LoadingSpinner /> : "Submit"}
+          {btnLoading ? <LoadingSpinner /> : "Login"}
         </button>
       </form>
     </div>
